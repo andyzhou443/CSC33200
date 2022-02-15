15acfe5a8e79b48b6769b599bfc677f9bb1a7649
Andy Zhou <andyzhou443@gmail.com>
2022-02-14 21:07:59 -0500

    Added provided files and completed part0a.c

---
+++ Lab 1/part0a.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h> 
+
+int main (int argc, char* argv[]) 
+{ 
+    char* filepath = argv[1]; 
+    int returnval; 
+
+    // Check file existence 
+    returnval = access (filepath, F_OK); 
+    if (returnval == 0) 
+        printf ("\n %s exists\n", filepath); 
+    else 
+    { 
+        if (errno == ENOENT)
+            printf ("%s does not exist\n", filepath); 
+        else if (errno == EACCES) 
+            printf ("%s is not accessible\n", filepath); 
+        return 0;
+    } 
+    
+    // Check read access ... 
+    returnval = access (filepath, R_OK);
+    if (returnval == 0)
+        printf ("\n %s read permissions is accessible" , filepath);
+    else{
+        printf ("\n %s read permissions is not accessible" , filepath);
+    }
+    // Check write access ...
+    returnval = access (filepath, W_OK);
+    if (returnval == 0)
+        printf ("\n %s write permissions is accessible" , filepath);
+    else{
+        printf ("\n %s write permissions is not accessible" , filepath);
+    }
+
+    return 0;
+}
